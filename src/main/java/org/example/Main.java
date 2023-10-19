bf3666781ca9d9e3d5707f8d74cd6a3f9243ef21
Gabriel <gabrielvalverde83@hotmail.com>
2023-10-18 21:50:43 -0300

    a ver si anda automatico

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        //modificando a ver si anda automatico
         System.out.printf("Hello and welcome!");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
